d670784a4a01affce9bbb8638b53f793d9fd6631
Nick C. Kaye <nick.c.kaye@gmail.com>
2014-11-20 00:50:56 +0100

    Generate default generic waves.

---
+++ mu/lib/log.js
@@ -0,0 +1,53 @@
+/**
+ * @author Nick Kaye <nick.c.kaye@gmail.com>
+ * @laboratory Outright Mental Inc.
+ */
+var LEVEL_DEBUG = true,
+    LEVEL_INFO = true,
+    LEVEL_WARN = true,
+    LEVEL_ERROR = true;
+
+/* global module */
+module.exports = {
+
+    /**
+     * Log @ Debug Level
+     * @param message
+     */
+    debug: function (message) {
+        if(LEVEL_DEBUG) {
+            console.log(message);
+        }
+    },
+
+    /**
+     * Log @ Info Level
+     * @param message
+     */
+    info: function (message) {
+        if(LEVEL_INFO) {
+            console.log(message);
+        }
+    },
+
+    /**
+     * Log @ Warn Level
+     * @param message
+     */
+    warn: function (message) {
+        if(LEVEL_WARN) {
+            console.log(message);
+        }
+    },
+
+    /**
+     * Log @ Error Level
+     * @param message
+     */
+    error: function (message) {
+        if(LEVEL_ERROR) {
+            console.log(message);
+        }
+    }
+
+};
