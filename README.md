50e7529a4c4aa5d48017427dddf23531b544277e
Nick C. Kaye <nick.c.kaye@gmail.com>
2014-11-20 05:07:46 +0100

    It is totally not possible to produce MU, in the G.E.B. example.

---
+++ README.md
@@ -0,0 +1,23 @@
+[![MIT License][license-image]][license-url] [![Build Status][travis-image]][travis-url]
+
+Experiments pertaining to Godel-Escher-Bach "Can you produce MU?" example.
+
+Author: [Nick C. Kaye](http://www.nickkaye.com)
+
+## Documentation
+
+TODO: Write This
+
+### Basic Usage
+
+TODO: Write This
+   
+## License
+
+HTTP Model-as-a-Service is freely distributable under the terms of the [MIT license](LICENSE).
+
+[license-image]: http://img.shields.io/badge/license-MIT-blue.svg?style=flat
+[license-url]: LICENSE
+
+[travis-url]: http://travis-ci.org/nickckaye/geb-mu
+[travis-image]: http://img.shields.io/travis/nickckaye/geb-mu/master.svg?style=flat
---
+++ main.js
@@ -0,0 +1,37 @@
+/**
+ * @author Nick Kaye <nick.c.kaye@gmail.com>
+ * @laboratory Outright Mental Inc.
+ */
+var Node = require('./lib/node'),
+  Log = require('./lib/etc/log');
+
+var
+// # of waves of mutations to simulate
+  mutation_waves = 10,
+// stop mutating any strings past this length
+  max_content_length = 10,
+  INITIAL_CONTENT = 'MI',
+  TARGET_CONTENT = 'MU';
+
+// first, the root node
+var r = new Node(INITIAL_CONTENT),
+  id_set = [],
+  found_target = {};
+
+// log the start
+r.save();
+
+// each wave of mutations
+for (var _i = 0; _i < mutation_waves; _i++) {
+  id_set = Node.find_all();
+  for (var _k in id_set) {
+    if (id_set[_k].content === TARGET_CONTENT) {
+      found_target[_k] = id_set[_k];
+    } else if (id_set[_k].content.length < max_content_length) {
+      id_set[_k].spawn_all_mutations();
+    }
+  }
+}
+
+// results
+Log.info(['FOUND TARGETS', found_target]);
